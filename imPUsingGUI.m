934225537d7b80a88ee7cbd1dd21e57c1d9fc468
shoumanx <mshoman01020@gmail.com>
2023-12-19 16:04:29 +0200

    Edits: All Buttons, Filters function, Brightness functions

--- imPUsingGUI.m
+++ imPUsingGUI.m
@@ -21,8 +21,14 @@ end
 
 function imPUsingGUI_OpeningFcn(hObject, eventdata, handles, varargin)
 
-handles.output = hObject;
+% Store the handle of the editable text box in the handles structure
+    handles.editBox = findobj('Tag', 'setBrightness');
+    set(handles.editBox, 'Callback', 'editBox_Callback');
+    guidata(hObject, handles);
 
+    handles.output = hObject;
+
+    
 % Update handles structure
 guidata(hObject, handles);
 
@@ -34,7 +40,13 @@ function varargout = imPUsingGUI_OutputFcn(hObject, eventdata, handles)
 varargout{1} = handles.output;
 
 
-
+function editBox_Callback(hObject, ~, handles)
+    global myValue;
+    myValue = str2double(get(hObject, 'String'));
+    disp(['Value updated to: ', num2str(myValue)]);
+    
+    
+    
 %function anticlockwise_Callback(hObject, eventdata, handles)
 
 %a=getappdata(0,'a');
@@ -53,7 +65,6 @@ varargout{1} = handles.output;
 
 
 
-
 % --- Executes on button press in hist.
 function hist_Callback(hObject, eventdata, handles)
 % hObject    handle to hist (see GCBO)
@@ -62,8 +73,26 @@ function hist_Callback(hObject, eventdata, handles)
 a=getappdata(0,'a');
 ahist=a;
 ahist=rgb2gray(ahist);
-axes(handles.axes1);
+
+% Create a new figure and set its size
+figure('Position', [20 100 1500 500]);
+
+% Display the original image in subplot 1
+subplot(1,3,1);
+imshow(a);
+title('Original Image');
+
+% Display the GrayScale image in subplot 1
+subplot(1,3,2);
+imshow(ahist);
+title('Grayscale Image');
+
+% Display the histogram in subplot 2
+subplot(1,3,3);
 imhist(ahist);
+title('Histogram');
+
+
 
 % --- Executes on button press in complement.
 function complement_Callback(hObject, eventdata, handles)
@@ -78,14 +107,86 @@ imshow(acomp);
 
 % --- Executes on button press in edge.
 function edge_Callback(hObject, eventdata, handles)
+    % Get the image data
+    a = getappdata(0,'a');
+    
+    %{
+    % Convert the image to grayscale if it is not already
+    if size(a, 3) == 3
+        a = rgb2gray(a);
+    end
+    
+    % Apply the Sobel filter
+    aedge = edge(a, 'Sobel');
+    %}
+    sobel_x = [-1 0 1; -2 0 2; -1 0 1];
+    sobel_y = [-1 -2 -1; 0 0 0; 1 2 1];
+    
+    a = rgb2gray(a);
+    aedge = imfilter(a, sobel_x);
+    aedge = imfilter(aedge, sobel_y);
+    % Display the result
+    axes(handles.axes1);
+    %imshow(aedge);
+    % Create a new figure and set its size
+    figure('Position', [100 100 1000 500]);
+    
+    % Display the original image in subplot 1
+    subplot(1,2,1);
+    %imshow(a);
+    title('Original Image');
+    
+    % Display the edge detected image in subplot 2
+    subplot(1,2,2);
+    imshow(aedge);
+    title('Edge Detected Image (Sobel_X adn Sobel_Y)');
 
-a=getappdata(0,'a');
-aedge=a;
-aedge=rgb2gray(aedge);
-aedge=edge(aedge,'Canny')'
-axes(handles.axes1);
-imshow(aedge);
-
+    
+function sobelX_edge_Callback(hObject, eventdata, handles)
+    
+    a = getappdata(0,'a');
+    sobel_x = [-1 0 1; -2 0 2; -1 0 1];    
+    a = rgb2gray(a);
+    aedge = imfilter(a, sobel_x);
+    % Display the result
+    axes(handles.axes1);
+    %imshow(aedge);
+    % Create a new figure and set its size
+    figure('Position', [100 100 1000 500]);
+    
+    % Display the original image in subplot 1
+    subplot(1,2,1);
+    imshow(a);
+    title('Original Image');
+    
+    % Display the edge detected image in subplot 2
+    subplot(1,2,2);
+    imshow(aedge);
+    title('Edge Detected Image (Sobel_X)');
+    
+    
+function sobelY_edge_Callback(hObject, eventdata, handles)
+    
+    a = getappdata(0,'a');
+    sobel_y = [-1 -2 -1; 0 0 0; 1 2 1];
+    a = rgb2gray(a);
+    aedge = imfilter(a, sobel_y);
+    % Display the result
+    axes(handles.axes1);
+    %imshow(aedge);
+    % Create a new figure and set its size
+    figure('Position', [100 100 1000 500]);
+    
+    % Display the original image in subplot 1
+    subplot(1,2,1);
+    imshow(a);
+    title('Original Image');
+    
+    % Display the edge detected image in subplot 2
+    subplot(1,2,2);
+    imshow(aedge);
+    title('Edge Detected Image (Sobel_Y)');
+    
 function uploadimage_Callback(hObject, eventdata, handles)
 
 a=uigetfile('.jpg')
@@ -148,7 +249,10 @@ function increase_brightness_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 a=getappdata(0,'a');
-brightness_value=50%str2double(get(handles.brightness_value,'String'));
+
+global myValue;
+brightness_value = myValue;
+disp(brightness_value);
 brighter_a=a+brightness_value;
 
 % Create a new figure and set its size
@@ -171,7 +275,8 @@ function decrease_brightness_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 a=getappdata(0,'a');
-brightness_value=50%str2double(get(handles.brightness_value,'String'));
+global myValue;
+brightness_value = myValue;
 darker_a=a-brightness_value;
 
 % Create a new figure and set its size
@@ -186,3 +291,25 @@ title('Original Image');
 subplot(1,2,2);
 imshow(darker_a);
 title('Darker Image');
+
+
+% --- Executes on slider movement.
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
